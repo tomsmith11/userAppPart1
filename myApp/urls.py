71caa95dfdb0816af8f43dc30633465069113cb6
Tom Smith <tom@craftscale.uk>
2025-01-17 13:29:40 +0000

    Added comment to urls.py

--- myApp/urls.py
+++ myApp/urls.py
@@ -19,6 +19,8 @@ from django.urls import path
 
 from . import views
 
+# Defined URL's for the project
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
